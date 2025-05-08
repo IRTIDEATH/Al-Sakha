fdc4342161c8dc77b05212fe66d975ff546ffece
IRTIDEATH <enderjhass@gmail.com>
2025-05-08 20:51:18 +0700

    Update font weight and suram colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistMono.variable} ${robotoMono.variable} antialiased`}
+        className={`${geistMono.variable} ${robotoMono.variable} antialiased font-normal`}
       >
         {children}
       </body>
