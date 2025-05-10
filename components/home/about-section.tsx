f0fd97a6186ad51cf9dd8445b6bdbb43d3a91276
IRTIDEATH <enderjhass@gmail.com>
2025-05-10 22:34:27 +0700

    Update about section (#1)

--- components/home/about-section.tsx
+++ components/home/about-section.tsx
@@ -6,7 +6,7 @@ const AboutSection = () => {
       <h2 className="text-2xl mb-6 font-medium">what about me?</h2>
       <p className="w-full text-zeta font-medium text-lg text-start">
         yea, an ordinary npc and{" "}
-        <span className="text-coffee">weak in math</span>, that's me. you can
+        <span className="text-coffee">weak in math</span>, that{"'"}s me. you can
         call me al sakha. sometimes i think about myself, who somehow got into
         programming. focusing on{" "}
         <span className="text-coffee">web development</span>, and hoping to
