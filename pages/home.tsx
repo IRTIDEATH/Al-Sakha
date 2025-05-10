d2a4f3c3ea83ff7977101bb31954850cf91bec67
IRTIDEATH <enderjhass@gmail.com>
2025-05-10 22:39:37 +0700

    Add about & internship section into the home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import HeroSection from "@/components/home/hero-section";
+import AboutSection from "@/components/home/about-section";
+
 import Divider from "@/components/ui/divider";
+import CardInternship from "@/components/card-internship";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <Divider />
+      <AboutSection />
+      <Divider />
+      <section className="mb-16">
+        <h2 className="text-2xl mb-6 font-medium">internship activities</h2>
+        <CardInternship />
+      </section>
     </>
   );
 };
