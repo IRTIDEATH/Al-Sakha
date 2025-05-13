790321964a3e73802a57fdfcfafe5a1033ae89ae
IRTIDEATH <enderjhass@gmail.com>
2025-05-14 00:52:22 +0700

    Create an project card section for the projects page

---
+++ components/section/projects/card-project-section.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import Link from "next/link";
+import { Badge } from "@/components/ui/skill-badge";
+import { Icon } from "@iconify/react";
+import {
+  MorphingDialog,
+  MorphingDialogTrigger,
+  MorphingDialogContent,
+  MorphingDialogClose,
+  MorphingDialogImage,
+  MorphingDialogContainer,
+} from "@/components/ui/morphing-dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Divider from "@/components/ui/divider";
+import { projects } from "@/constants/index";
+
+const CardProjectSecton = () => {
+  return (
+    <>
+      {projects.map((item, index) => (
+        <div key={index} className="mt-12">
+          <div className="flex w-full">
+            <div className="relative hidden md:block mr-5">
+              <MorphingDialog
+                transition={{
+                  duration: 0.3,
+                  ease: "easeInOut",
+                }}
+              >
+                <MorphingDialogTrigger>
+                  <div className="relative h-40 w-28">
+                    <MorphingDialogImage
+                      src={item.image}
+                      alt="Sony Style Store in the Sony Center complex - Berlin, Germany (2000)"
+                      className="object-cover"
+                    />
+                  </div>
+                </MorphingDialogTrigger>
+                <MorphingDialogContainer>
+                  <MorphingDialogContent className="relative">
+                    <div className="relative h-[90vh] w-[90vw]">
+                      <MorphingDialogImage
+                        src={item.image}
+                        alt="Sony Style Store in the Sony Center complex - Berlin, Germany (2000)"
+                        className="object-contain"
+                      />
+                    </div>
+                  </MorphingDialogContent>
+                  <MorphingDialogClose
+                    className="fixed right-6 top-6 h-fit w-fit rounded-full bg-white p-1"
+                    variants={{
+                      initial: { opacity: 0 },
+                      animate: {
+                        opacity: 1,
+                        transition: { delay: 0.3, duration: 0.1 },
+                      },
+                      exit: { opacity: 0, transition: { duration: 0 } },
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M18 6 6 18" />
+                      <path d="m6 6 12 12" />
+                    </svg>
+                  </MorphingDialogClose>
+                </MorphingDialogContainer>
+              </MorphingDialog>
+            </div>
+            <div>
+              <h1 className="mb-2 text-[26px] font-medium">{item.name}</h1>
+              <p className="mb-6 text-zeta text-[16px] font-medium">
+                {item.description}
+              </p>
+              <div className="w-full flex flex-col-reverse md:flex-row gap-4 md:gap-0 items-start justify-between">
+                <div className="flex items-center gap-3">
+                  {item.view ? (
+                    <>
+                      <Link
+                        href={item.view ?? ""}
+                        className="text-sm font-medium text-foreground hover:underline"
+                      >
+                        view
+                      </Link>
+                      <span className="text-zeta">|</span>
+                    </>
+                  ) : null}
+                  {item.github ? (
+                    <>
+                      <Link
+                        href={item.github ?? ""}
+                        className="text-sm font-medium text-foreground hover:underline"
+                      >
+                        github
+                      </Link>
+                      <span className="text-zeta">|</span>
+                    </>
+                  ) : null}
+                  <span className="bg-foreground px-3.5 py-1 text-sm text-background">
+                    {item.status}
+                  </span>
+                </div>
+                <div className="flex flex-wrap items-center justify-end gap-2 h-full">
+                  {item.technologies.map((items, index) => (
+                    <TooltipProvider delayDuration={100} key={index}>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Badge>
+                            <Icon
+                              className="-ms-0.5 text-[16px]"
+                              icon={items.icon}
+                            />
+                          </Badge>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{items.name}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <Divider />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CardProjectSecton;
--- constants/index.ts
+++ constants/index.ts
@@ -16,6 +16,16 @@ interface Iinternship {
     link: string;
 };
 
+interface Iproject {
+    image: string;
+    name: string;
+    description: string;
+    view?: string;
+    github?: string;
+    status: string;
+    technologies: IskillBadgeIcon[];
+}
+
 export const navigationItems: NavigationItemProps[] = [
     { name: "home", href: "/" },
     { name: "projects", href: "/projects" },
@@ -127,3 +137,146 @@ export const internships: Iinternship[] = [
         link: "https://gamecomm.vercel.app/",
     },
 ];
+
+export const projects: Iproject[] = [
+    {
+        image: "/project/pancarimaimg.webp",
+        name: "Pancarima",
+        description: "Pancarima is a website that provides information related to tourist attractions in Indonesia. Created during the Sagasitas February 2024 competition.",
+        view: "https://pancarima.vercel.app/",
+        status: "Completed",
+        technologies: [
+            {
+                name: "Javascript",
+                icon: "simple-icons:javascript"
+            },
+            {
+                name: "React.js",
+                icon: "simple-icons:react"
+            },
+            {
+                name: "Three.js",
+                icon: "simple-icons:threedotjs"
+            },
+            {
+                name: "Tailwind CSS",
+                icon: "simple-icons:tailwindcss"
+            },
+        ],
+    },
+    {
+        image: "/project/zabodimg.webp",
+        name: "Zabod",
+        description: "Zabod is a keyboard online store platform where buyers can customize the keyboard as desired.",
+        status: "Development",
+        technologies: [
+            {
+                name: "Typescript",
+                icon: "simple-icons:typescript"
+            },
+            {
+                name: "Next.js",
+                icon: "simple-icons:nextdotjs"
+            },
+            {
+                name: "Tanstack Query",
+                icon: "simple-icons:reactquery"
+            },
+            {
+                name: "Tailwind CSS",
+                icon: "simple-icons:tailwindcss"
+            },
+            {
+                name: "Shadcn/ui",
+                icon: "simple-icons:shadcnui"
+            },
+            {
+                name: "PostgreSQL",
+                icon: "simple-icons:postgresql"
+            },
+            {
+                name: "PrismaORM",
+                icon: "simple-icons:prisma"
+            },
+        ],
+    },
+    {
+        image: "/project/inditechimg.webp",
+        name: "INDI Technology",
+        description: "INDI Tech is a unique and interactive 3D-based company profile website of PT Indi Technology. Created during pre-internship activities.",
+        view: "https://web.indi.tech/",
+        status: "Completed",
+        technologies: [
+            {
+                name: "Javascript",
+                icon: "simple-icons:javascript"
+            },
+            {
+                name: "React.js",
+                icon: "simple-icons:react"
+            },
+            {
+                name: "Three.js",
+                icon: "simple-icons:threedotjs"
+            },
+            {
+                name: "Tailwind CSS",
+                icon: "simple-icons:tailwindcss"
+            },
+        ],
+    },
+    {
+        image: "/project/zentraimg.webp",
+        name: "ZentraHealth",
+        description: "ZentraHealth is a website that provides information related to a healthy lifestyle. Created during the Sagasitas August 2024 competition.",
+        view: "https://zentrahealth.vercel.app/",
+        status: "Completed",
+        technologies: [
+            {
+                name: "Javascript",
+                icon: "simple-icons:javascript"
+            },
+            {
+                name: "React.js",
+                icon: "simple-icons:react"
+            },
+            {
+                name: "Three.js",
+                icon: "simple-icons:threedotjs"
+            },
+            {
+                name: "Tailwind CSS",
+                icon: "simple-icons:tailwindcss"
+            },
+            {
+                name: "Shadcn/ui",
+                icon: "simple-icons:shadcnui"
+            },
+        ],
+    },
+    {
+        image: "/project/geoporimg.webp",
+        name: "Geoportal Laut Berkah",
+        description: "An interactive 3D-based platform related to the licensing of marine space utilization letters for Central Kalimantan province. Created during internship activities.",
+        view: "https://web.geoportal-lautberkah.kalteng.go.id/",
+        status: "Completed",
+        technologies: [
+            {
+                name: "Javascript",
+                icon: "simple-icons:javascript"
+            },
+            {
+                name: "React.js",
+                icon: "simple-icons:react"
+            },
+            {
+                name: "Three.js",
+                icon: "simple-icons:threedotjs"
+            },
+            {
+                name: "Tailwind CSS",
+                icon: "simple-icons:tailwindcss"
+            },
+        ],
+    },
+]
