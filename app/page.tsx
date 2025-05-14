70613fb1d7dcf40b8a643f0d5bceeec8fe87bee9
IRTIDEATH <enderjhass@gmail.com>
2025-05-14 23:42:19 +0700

    Refactor home and project pages into components; remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
-import HomePage from "@/pages/home-page";
+"use client";
 
-const Home = () => {
-  return <HomePage />;
+import HeroSection from "@/components/section/home/hero-section";
+import AboutSection from "@/components/section/home/about-section";
+import Divider from "@/components/ui/divider";
+import ColorPaletteSection from "@/components/section/home/color-palette-section";
+import InspirationSection from "@/components/section/home/inspiration-section";
+import dynamic from "next/dynamic";
+import { useReducedMotion } from "motion/react";
+
+const InternshipSection = dynamic(() => import("@/components/section/home/internship-section"));
+
+const Page = () => {
+  const prefersReducedMotion = useReducedMotion();
+
+  return (
+    <>
+      <HeroSection />
+      <Divider />
+      <AboutSection />
+      <Divider />
+      {!prefersReducedMotion && <InternshipSection />}
+      <Divider />
+      <ColorPaletteSection />
+      <Divider />
+      <InspirationSection />
+    </>
+  );
 };
 
-export default Home;
+export default Page;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,19 @@
-import ProjectPage from "@/pages/project-page";
+import CardProjectSecton from "@/components/section/projects/card-project-section";
+import TechStackSection from "@/components/section/projects/tech-stack-section";
+import TitleSection from "@/components/section/projects/title-section";
+import Divider from "@/components/ui/divider";
 
-const Projects = () => {
-  return <ProjectPage />;
+const Page = () => {
+  return (
+    <>
+      <TitleSection />
+      <TechStackSection />
+      <Divider />
+      <CardProjectSecton />
+    </>
+  );
 };
 
-export default Projects;
+export default Page;
 
 export const dynamic = "force-static";
