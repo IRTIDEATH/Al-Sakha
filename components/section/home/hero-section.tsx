d160569c886d925fd6841e8f49a7809cbfce4a27
IRTIDEATH <enderjhass@gmail.com>
2025-05-17 18:51:14 +0700

    Add animated cursor component and integrate into hero section

--- components/section/home/hero-section.tsx
+++ components/section/home/hero-section.tsx
@@ -1,19 +1,53 @@
-"use client"
+"use client";
 
 import { skillBadgeIcon } from "@/constants";
 import { AnimatedButton } from "@/components/ui/button";
 import { Badge } from "@/components/ui/skill-badge";
 import { Icon } from "@iconify/react";
 import CoffeeText from "@/components/ui/coffee-text";
+import { Cursor } from "@/components/ui/animate-cursor";
+import { useReducedMotion } from "motion/react";
+import Image from "next/image";
+
 
 const HeroSection = () => {
+  const prefersReducedMotion = useReducedMotion();
   return (
     <>
       <section className="mt-12 flex flex-col md:flex-row items-start md:items-center gap-8 justify-between">
         <div className="space-y-6">
           <h1 className="text-3xl sm:text-4xl font-medium">
-            <span className="text-zeta">Gray,</span> cozy as a <br /> cat taking a
-            nap.
+            <span className="text-zeta">
+              {!prefersReducedMotion && (
+                <Cursor
+                  attachToParent
+                  variants={{
+                    initial: { height: 0, opacity: 0, scale: 0.3 },
+                    animate: { height: "auto", opacity: 1, scale: 1 },
+                    exit: { height: 0, opacity: 0, scale: 0.3 },
+                  }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.3,
+                    bounce: 0.1,
+                  }}
+                  className="overflow-hidden"
+                  springConfig={{
+                    bounce: 0.01,
+                  }}
+                >
+                  <Image
+                    src="/zetaagent.png"
+                    alt="Vestia Zeta"
+                    width={240}
+                    height={240}
+                    className="h-40 w-60 object-cover"
+                  />
+                </Cursor>
+              )}
+              Gray,
+            </span>{" "}
+            cozy as a <br /> cat taking a nap.
           </h1>
           <p className="font-medium text-zeta text-lg">
             loving cold or hot <CoffeeText>coffee</CoffeeText>
@@ -31,10 +65,7 @@ const HeroSection = () => {
           <div className="md:w-[21rem] space-x-[4px] space-y-[4px]">
             {skillBadgeIcon.map((item, index) => (
               <Badge key={index}>
-                <Icon
-                  className="-ms-0.5 text-[16px]"
-                  icon={item.icon}
-                />
+                <Icon className="-ms-0.5 text-[16px]" icon={item.icon} />
                 {item.name}
               </Badge>
             ))}
@@ -43,6 +74,6 @@ const HeroSection = () => {
       </section>
     </>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
