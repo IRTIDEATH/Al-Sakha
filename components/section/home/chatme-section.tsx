bd1c299c3505b3e05cdebcef4be86ff49c6d417d
IRTIDEATH <enderjhass@gmail.com>
2025-05-26 14:42:09 +0700

    Add linkedin link in chatme section

--- components/section/home/chatme-section.tsx
+++ components/section/home/chatme-section.tsx
@@ -7,7 +7,9 @@ const ChatmeSection = () => {
     <>
       <section>
         <h1 className="text-2xl mb-6 font-medium">wanna talk?</h1>
-        <p className="w-full text-zeta font-medium text-lg text-start">need help or talk about random things</p>
+        <p className="w-full text-zeta font-medium text-lg text-start">
+          need help, talk about random things, and connect with me.
+        </p>
         <ul className="w-full text-zeta font-medium text-lg text-start mt-4">
           <li className="flex items-center gap-2">
             <span className="text-2xl text-foreground">{"-->"}</span>
@@ -27,6 +29,16 @@ const ChatmeSection = () => {
               className="hover:underline decoration-2"
             >
               Instagram
+            </Link>
+          </li>
+          <li className="flex items-center gap-2">
+            <span className="text-2xl text-foreground">{"-->"}</span>
+            <Link
+              aria-label="Talk with me on Linkedin"
+              href="https://www.linkedin.com/in/al-sakha-1214ba366/"
+              className="hover:underline decoration-2"
+            >
+              Linkedin
             </Link>
           </li>
         </ul>
