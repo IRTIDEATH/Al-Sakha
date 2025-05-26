eb86ad0e31ad361642d6a17511bdc82a2508e0cd
IRTIDEATH <enderjhass@gmail.com>
2025-05-26 20:05:52 +0700

    Update hero section by adding a clock and improved the skill badge layout

--- components/section/home/hero-section.tsx
+++ components/section/home/hero-section.tsx
@@ -9,15 +9,30 @@ import { useReducedMotion } from "motion/react";
 import Image from "next/image";
 import ZetaText from "@/components/ui/zeta-text";
 import dynamic from "next/dynamic";
+import { SlidingNumber } from "@/components/ui/sliding-number";
+import { useEffect, useState } from "react";
 
 const Cursor = dynamic(() => import("@/components/ui/animate-cursor"), {ssr: false});
 
 const HeroSection = () => {
   const prefersReducedMotion = useReducedMotion();
+  const [hours, setHours] = useState(new Date().getHours());
+  const [minutes, setMinutes] = useState(new Date().getMinutes());
+  const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHours(new Date().getHours());
+      setMinutes(new Date().getMinutes());
+      setSeconds(new Date().getSeconds());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <section className="mt-12 flex flex-col md:flex-row items-start md:items-center gap-8 justify-between">
-        <div className="space-y-6">
+        <div className="space-y-6 flex flex-col items-start">
           <h1 className="text-3xl sm:text-4xl font-medium">
             <ZetaText>
               {!prefersReducedMotion && (
@@ -48,8 +63,8 @@ const HeroSection = () => {
                   />
                 </Cursor>
               )}
-              Gray,
-            </ZetaText>{" "}
+              Gray
+            </ZetaText>,{" "}
             cozy as a <br /> cat taking a nap.
           </h1>
           <p className="font-medium text-zeta text-lg">
@@ -57,7 +72,15 @@ const HeroSection = () => {
             <br />
             while working and study.
           </p>
-          <AnimatedButton>Download CV</AnimatedButton>
+          <div className="px-3 py-1 border-2 border-zeta">
+            <div className="flex items-center gap-0.5 font-roboto text-zeta">
+              <SlidingNumber value={hours} padStart={true} />
+              <span>:</span>
+              <SlidingNumber value={minutes} padStart={true} />
+              <span>:</span>
+              <SlidingNumber value={seconds} padStart={true} />
+            </div>
+          </div>
         </div>
         <div className="bg-foreground p-5 flex flex-col">
           <ul className="flex mb-4 gap-2">
@@ -65,7 +88,7 @@ const HeroSection = () => {
             <li className="w-3 h-3 bg-white rounded-full"></li>
             <li className="w-3 h-3 bg-white rounded-full"></li>
           </ul>
-          <div className="md:w-[21rem] space-x-[4px] space-y-[4px]">
+          <div className="w-full md:max-w-[21rem] space-x-[4px] space-y-[4px]">
             {skillBadgeIcon.map((item, index) => (
               <Badge key={index}>
                 <Icon className="-ms-0.5 text-[16px]" icon={item.icon} />
