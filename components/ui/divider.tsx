8fddf5e320db09e803f5fed7882f07dd153c9000
IRTIDEATH <enderjhass@gmail.com>
2025-06-02 20:47:15 +0700

    Fix error no empty object type on divider component

--- components/ui/divider.tsx
+++ components/ui/divider.tsx
@@ -3,9 +3,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-export interface DividerProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const Divider = ({ className }: DividerProps) => {
+const Divider = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
   return <div className={cn("bg-suram py-[1.5px] w-full my-12", className)} />;
 };
 
