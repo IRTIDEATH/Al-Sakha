bf697584a8e9d6ee5d555b8332ee48aef80f6474
IRTIDEATH <enderjhass@gmail.com>
2025-06-20 21:01:33 +0700

    Add color palette section with new style

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import ChatmeSection from "@/components/section/home/chatme-section";
 import ServiceSection from "@/components/section/home/service-section";
 import InternshipSection from "@/components/section/home/internship-section";
 import SkillSection from "@/components/section/home/skill-section";
+import ColorPaletteSection from "@/components/section/home/color-palette-section";
 
 const Page = () => {
-
   return (
     <main>
       <HeroSection />
@@ -20,6 +20,8 @@ const Page = () => {
       <InternshipSection />
       <Divider />
       <ServiceSection />
+      <Divider />
+      <ColorPaletteSection/>
       <Divider />
       <InspirationSection />
       <Divider />
---
+++ components/section/home/color-palette-section.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { toast } from "sonner";
+
+interface Icolors {
+  name: string;
+  class: string;
+  hex: string;
+}
+
+const ColorPaletteSection = () => {
+
+  const colors: Icolors[] = [
+    { name: "background", class: "text-background", hex: "#F2F2F4" },
+    { name: "foreground", class: "text-foreground", hex: "#2E2E2E" },
+    { name: "coffee", class: "text-coffee", hex: "#8C6053" },
+    { name: "zeta", class: "text-zeta", hex: "#686C6F" },
+    { name: "suram", class: "text-suram", hex: "#E6E8ED" },
+  ];
+
+  const handleCopyColor = (hex: string) => {
+    navigator.clipboard.writeText(hex);
+    toast.success("Color copied to clipboard", {
+      description: `Color ${hex} copied to clipboard`,
+      duration: 3000,
+    });
+  };
+  return (
+    <>
+      <section>
+        <h1 className="text-2xl mb-4 font-medium">color palettes</h1>
+        <p className="w-full text-zeta font-medium text-[16px] text-start">
+          little and convenient. click to copy the color code.
+        </p>
+        <ul className="w-full text-zeta font-medium text-lg text-start mt-4">
+          {colors.map((item, index) => (
+            <li className="flex items-center gap-2" key={index}>
+              <span className={`text-2xl ${item.class}`}>{"-->"}</span>
+              <p
+                onClick={() => handleCopyColor(item.hex)}
+                className="hover:underline decoration-2 cursor-pointer"
+              >
+                {item.name}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default ColorPaletteSection;
