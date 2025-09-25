e5cbc00099a82120de03598c93992e8b5878b889
IRTIDEATH <enderjhass@gmail.com>
2025-09-25 17:37:40 +0700

    Fix remove reducemotion

--- components/pages/project/card-project-section.tsx
+++ components/pages/project/card-project-section.tsx
@@ -19,72 +19,68 @@ import {
 } from "@/components/ui/tooltip";
 import { Divider } from "@/components/ui/divider";
 import { projects } from "@/constants/index";
-import { useReducedMotion } from "motion/react";
 
 const CardProjectSecton = () => {
-  const prefersReducedMotion = useReducedMotion();
   return (
     <>
       {projects.map((item, index) => (
         <section key={index}>
           <div className="flex w-full items-start gap-0 md:gap-5">
             <div className="relative hidden md:block">
-              {!prefersReducedMotion && (
-                <MorphingDialog
-                  transition={{
-                    duration: 0.3,
-                    ease: "easeInOut",
-                  }}
-                >
-                  <MorphingDialogTrigger>
-                    <div className="relative h-40 w-28">
-                      <div className="w-full h-full bg-foreground hover:shadow-shadow z-10 absolute hover:bg-transparent ease-in-out transition-all duration-400" />
+              <MorphingDialog
+                transition={{
+                  duration: 0.3,
+                  ease: "easeInOut",
+                }}
+              >
+                <MorphingDialogTrigger>
+                  <div className="relative h-40 w-28">
+                    <div className="w-full h-full bg-foreground hover:shadow-shadow z-10 absolute hover:bg-transparent ease-in-out transition-all duration-400" />
+                    <MorphingDialogImage
+                      src={item.image}
+                      alt={item.name}
+                      className="object-cover"
+                    />
+                  </div>
+                </MorphingDialogTrigger>
+                <MorphingDialogContainer>
+                  <MorphingDialogContent className="relative">
+                    <div className="relative h-[90vh] w-[90vw]">
                       <MorphingDialogImage
                         src={item.image}
                         alt={item.name}
-                        className="object-cover"
+                        className="object-contain"
                       />
                     </div>
-                  </MorphingDialogTrigger>
-                  <MorphingDialogContainer>
-                    <MorphingDialogContent className="relative">
-                      <div className="relative h-[90vh] w-[90vw]">
-                        <MorphingDialogImage
-                          src={item.image}
-                          alt={item.name}
-                          className="object-contain"
-                        />
-                      </div>
-                    </MorphingDialogContent>
-                    <MorphingDialogClose
-                      className="fixed right-6 top-6 h-fit w-fit rounded-full bg-white p-1"
-                      variants={{
-                        initial: { opacity: 0 },
-                        animate: {
-                          opacity: 1,
-                          transition: { delay: 0.3, duration: 0.1 },
-                        },
-                        exit: { opacity: 0, transition: { duration: 0 } },
-                      }}
+                  </MorphingDialogContent>
+                  <MorphingDialogClose
+                    className="fixed right-6 top-6 h-fit w-fit rounded-full bg-white p-1"
+                    variants={{
+                      initial: { opacity: 0 },
+                      animate: {
+                        opacity: 1,
+                        transition: { delay: 0.3, duration: 0.1 },
+                      },
+                      exit: { opacity: 0, transition: { duration: 0 } },
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M18 6 6 18" />
-                        <path d="m6 6 12 12" />
-                      </svg>
-                    </MorphingDialogClose>
-                  </MorphingDialogContainer>
-                </MorphingDialog>
-              )}
+                      <path d="M18 6 6 18" />
+                      <path d="m6 6 12 12" />
+                    </svg>
+                  </MorphingDialogClose>
+                </MorphingDialogContainer>
+              </MorphingDialog>
             </div>
             <article className="w-full">
               <h2 className="mb-2 text-[24px] font-medium">{item.name}</h2>
